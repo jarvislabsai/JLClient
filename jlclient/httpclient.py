5e9474dbd775fba8890344470864c892f651f545
vishnu kumar <vishnu.techsource@gmail.com>
2021-12-17 18:22:47 +0530

    Adding new config files

---
+++ jlclient/httpclient.py
@@ -0,0 +1,29 @@
+import requests
+import urllib3
+import certifi
+import json
+url = "https://backendprod.jarvislabs.ai:8000/"
+
+http = urllib3.PoolManager(
+    cert_reqs="CERT_REQUIRED",
+    ca_certs=certifi.where()
+)
+
+
+def post(data, func):
+    encoded_body = json.dumps(data)
+    try:
+        r = http.request('POST', url+func,
+                         headers={'Content-Type': 'application/json'},
+                         body=encoded_body,
+                         # fields={'files': files}
+                         #   timeout=10
+                         )
+    except requests.exceptions.Timeout as e:
+        print(e)
+    return json.loads(r.data)
+
+
+def post_files(files, func):
+    r = requests.post(url+func, files=files)
+    return r.text
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='jlclient',
+      version='0.1',
+      description='API for interacting with Jarvislabs.ai for managing GPU powered instances',
+      url='https://github.com/jarvislabsai/jlclient',
+      author='Team Jarvislabs.ai',
+      author_email='hello@jarvislabs.ai',
+      license='MIT',
+      packages=['jlclient'],
+      zip_safe=False)
